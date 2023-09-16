173bfbc428563ea5cda9827682dbda503920b932
FireboltLegend <aik210000@utdallas.edu>
2023-09-16 14:24:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ model = SVC(kernel='rbf', C=1E10) # C adjusts sensitivity for misclassifications
 model.fit(X, y)
 
 def main():
-{
   arr = ["No Risk", "Low Risk", "Moderate Risk", "High Risk"]
   csv = np.genfromtext('Dataset_-_CBRE_Challenge_-_HackSMU_2023.csv', delimiter = ",")
   
+def formatData():
+  numArr
   
-}
+  return numArr
