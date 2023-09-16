36b28787e9aa99ba710c7091d643cf2ea6326241
AdithTalupuru <112774671+MonoxideFlames@users.noreply.github.com>
2023-09-16 15:47:12 -0500

    Update matrix.h

--- HackSMU Project Code/matrix.h
+++ HackSMU Project Code/matrix.h
@@ -5,6 +5,11 @@ typedef struct {
     fxp32_16 elements[16];
 } Matrix;
 
-void Matrix_mul(Matrix* out, Matrix* a, Matrix* b){
-    
-}
+void Matrix_mul(Matrix* out, Matrix* a, Matrix* b);
+void Matrix_add(Matrix* out, Matrix* a, Matrix* b);//Is safe to use destructively
+void Matrix_comp(Matrix* out, Matrix* in);
+void Matrix_inv(Matrix* out, Matrix* in);
+
+void Matrix_rot(Matrix* out, Vector* axis, fxp32_16 angle);
+void Matrix_reflect(Matrix* out, Vector* line, fxp32_16 bias);
+void Matrix_apply(Vector* out, Matrix* t, Vector* in);
