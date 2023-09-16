e4571a59ab3e845bb0f85b447746b5f6cdf6b390
FireboltLegend <61440403+FireboltLegend@users.noreply.github.com>
2023-09-16 13:35:53 -0500

    Update main.py
    Imported Libraries

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+import seaborn as sns; sns.set()
+from sklearn.svm import SVR
+model = SVC(kernel='rbf', C=1E10) # C adjusts sensitivity for misclassifications
+model.fit(X, y)
+
+def main()
+{
+  
+}
