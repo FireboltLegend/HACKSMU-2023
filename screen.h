c76a2d72a0efad4fab6847a5f0e2c7b14a4f6dcc
FireboltLegend <aik210000@utdallas.edu>
2023-09-17 11:37:47 -0500

    Bug Fixes

--- screen.h
+++ screen.h
@@ -11,7 +11,7 @@ typedef struct {
 
 //module interface
 Screen* Screen_make(int w, int h);
-Screen* Screen_delete(Screen* scr);
+void Screen_delete(Screen* scr);
 void Screen_point(Screen* scr, fxp32_16 x, fxp32_16 y, fxp32_16 brightness);
 void Screen_clear(Screen* scr);
 void Screen_show(Screen* scr);
